dd88b21b27e00c0527ebb7f923baeae2e40b670b
Sandesh <sandesh2643@gmail.com>
2017-09-21 14:27:09 +0530

    404.php commenting

--- 404.php
+++ 404.php
@@ -10,7 +10,20 @@
 
 ?>
 
-<?php add_action( 'astra_entry_content_404_page', 'astra_entry_content_404_page_template' ); ?>
+<?php
+
+/**
+ * 404 Content Template Part
+ *
+ * => Action hooked 'astra_entry_content_404_page'
+ *
+ * 'astra_entry_content_404_page_template' function found in "framework/structure/404.php"
+ *
+ * @since 1.0.0
+ */
+add_action( 'astra_entry_content_404_page', 'astra_entry_content_404_page_template' );
+
+?>
 
 <?php get_header(); ?>
 
